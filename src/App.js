8a0e54c4d25b1b91cce24c7061fb468bfbcaea0f
BasilBinoy <bailbinoy1401@gmail.com>
2024-02-09 01:07:06 +0530

    waste update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Entry from './components/Entry';
+import View from './components/View';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
       <Route path='/' element={<Navbar/>}/>
       <Route path='/' element={<Entry/>}/>
+      <Route path='/view' element={<View/>}/>
       </Routes>
       </BrowserRouter>
     </div>
